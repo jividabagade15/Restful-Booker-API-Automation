e884839150ba71e952882e9451e99ede5248ddd3
Jivida Bagade <jivibagade.2415@gmail.com>
2026-08-14 08:58:46 +0530

    Added GET booking by ID API test

--- src/test/java/stepdefinitions/BookingSteps.java
+++ src/test/java/stepdefinitions/BookingSteps.java
@@ -11,27 +11,55 @@ import io.cucumber.java.en.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-
 public class BookingSteps {
 	private Response response;
+	private int id;
+
 	@When("a GET request is sent to the booking endpoint")
 	public void a_get_request_is_sent_to_the_booking_endpoint() throws IOException {
-		RestAssured.baseURI= ConfigReader.getProperty("baseUrl");
-		response= given().log().all()
-		.when().get("/booking");
+		RestAssured.baseURI = ConfigReader.getProperty("baseUrl");
+		response = given().log().all().when().get("/booking");
 	}
 
 	@Then("the booking API responds with status code {int}")
 	public void the_booking_api_responds_with_status_code(Integer expectedStatusCode) {
-		response.then().log().all().extract().response();
-		Assert.assertEquals(response.getStatusCode(),expectedStatusCode,"Unexpected status code for get all bookings");
-		
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), expectedStatusCode, "Unexpected status code for booking request");
+
 	}
 
 	@Then("the response contains booking IDs")
 	public void the_response_contains_booking_IDs() {
-		List<Integer> bookingIds= response.jsonPath().getList("bookingid");
-		Assert.assertFalse(bookingIds.isEmpty(),"List for booking Ids should not be empty");
+		List<Integer> bookingIds = response.jsonPath().getList("bookingid");
+		Assert.assertFalse(bookingIds.isEmpty(), "List for booking Ids should not be empty");
 	}
 
+	@Given("a valid booking ID")
+	public void a_valid_booking_id() throws IOException {
+		RestAssured.baseURI = ConfigReader.getProperty("baseUrl");
+		response = given().log().all().when().get("/booking");
+		
+		List<Integer> bookingIds = response.jsonPath().getList("bookingid");
+		Assert.assertFalse(bookingIds.isEmpty(), "List for booking Ids should not be empty");
+
+		id = bookingIds.get(0);
+
+	}
+
+	@When("a GET request is sent to the booking endpoint using the booking ID")
+	public void a_get_request_is_sent_to_the_booking_endpoint_using_the_booking_id() throws IOException {
+		RestAssured.baseURI = ConfigReader.getProperty("baseUrl");
+		response = given().log().all().pathParam("id", id).when().get("/booking/{id}");
+
+	}
+
+	@Then("the response contains booking details")
+	public void the_response_contains_booking_details() {
+		String firstName = response.jsonPath().getString("firstname");
+		String totalPrice = response.jsonPath().getString("totalprice");
+
+		Assert.assertNotNull(firstName, "First name should be present in booking response");
+		Assert.assertNotNull(totalPrice, "The total price should be present in booking response");
+
+	}
 }
--- src/test/resources/features/booking.feature
+++ src/test/resources/features/booking.feature
@@ -4,3 +4,10 @@ Feature: Booking Retrieval
     When a GET request is sent to the booking endpoint
     Then the booking API responds with status code 200
     And the response contains booking IDs
+
+    
+   Scenario: Get Booking by ID
+   Given a valid booking ID
+   When a GET request is sent to the booking endpoint using the booking ID
+   Then the booking API responds with status code 200
+   And the response contains booking details
